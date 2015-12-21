dacddce82d0047c6e565b98d9e9fc34e1dfdcab8
Marco Castelluccio <mcastelluccio@mozilla.com>
2015-12-21 13:31:41 +0000

    Use oghliner to generate service worker for offlining

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,23 @@
 var gulp = require('gulp');
 var fse = require('fs-extra');
+var oghliner = require('oghliner');
 
-gulp.task('default', ['clean'], function() {
+gulp.task('default', ['clean', 'build', 'offline']);
+
+gulp.task('clean', function() {
+  fse.removeSync('dist');
+});
+
+gulp.task('build', function() {
   fse.mkdirSync('dist');
   fse.copySync('index.html', 'dist/index.html');
 });
 
-gulp.task('clean', function() {
-  fse.removeSync('dist');
+gulp.task('offline', function() {
+  return oghliner.offline({
+    rootDir: 'dist',
+    fileGlobs: [
+      'index.html',
+    ],
+  });
 });
